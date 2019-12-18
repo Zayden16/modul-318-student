658b96653df27f9fa03d35c28d936d509bc02565
Yazdan Musa <yazdanmusa@gmail.com>
2019-12-18 11:11:24 +0100

    auto generated code fix

--- src/SwissTransportGUI/SwissTransportApp.Designer.cs
+++ src/SwissTransportGUI/SwissTransportApp.Designer.cs
@@ -459,7 +459,7 @@
             // 
             this.Lbl_Uhr.AutoSize = true;
             this.Lbl_Uhr.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Uhr.Location = new System.Drawing.Point(1791, 24);
+            this.Lbl_Uhr.Location = new System.Drawing.Point(1818, 24);
             this.Lbl_Uhr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Uhr.Name = "Lbl_Uhr";
             this.Lbl_Uhr.Size = new System.Drawing.Size(246, 64);
